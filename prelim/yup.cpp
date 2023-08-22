d162f29b9fe5500835e914f11adf7dbabfad6777
onesixtwo <vargaskenneth789@gmail.com>
2023-08-22 22:46:53 +0800

    commit

---
+++ prelim/yup.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int n[3];
+    int a,val,put,s;
+
+    n[0] = 32;
+    n[1] = 12;
+    n[2] = 54;
+   
+    for(a=0; a < 3; a++)
+        cout << n[a] <<" ";
+       
+    cout << "\nenter number: ";
+    cin >> val;
+    
+    cout << "\nWhere to place?: ";
+    cin >> put;
+    
+    n[put] = val;
+    cout << "\nValues added";
+    
+    cout << "\n";
+    for(a=0; a < 3; a++)
+        cout << n[a] <<" "; 
+        
+    cout << "\nEnter number: ";
+    cin >> s;
+    
+    for (a = 0; a < 3; a++)
+        if (n[a] == s){
+            cout << " yes ";
+            cout << a;
+            break;
+            }
+        if (a == 3){
+            cout << " no ";
+        }
+
+}
